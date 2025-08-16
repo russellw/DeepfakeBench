2ed15e74d780166b2d7d22b6a0fbeab62450ef5f
Russell Wallace <russell.wallace@gmail.com>
2025-08-16 02:11:35 +0100

    Modified 1 file(s): test.bat

--- test.bat
+++ test.bat
@@ -1,4 +1,1 @@
-python training/test.py \
---detector_path ./training/config/detector/xception.yaml \
---test_dataset "UADFV" \
---weights_path ./training/weights/xception_best.pth
+python training/test.py --detector_path ./training/config/detector/xception.yaml --test_dataset "UADFV" --weights_path ./training/weights/xception_best.pth
