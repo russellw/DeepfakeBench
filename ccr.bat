84f07052b73db40df81e72f6f29fedf53e53e930
Russell Wallace <russell.wallace@gmail.com>
2025-08-16 11:34:36 +0100

    Modified 2 file(s): ccr.bat, ccx.bat

--- ccr.bat
+++ ccr.bat
@@ -1,3 +1,4 @@
 call conda create --name %1 python=3.7 -y
+Rem should this specify 3.7.12?
 conda activate %1
 
--- ccx.bat
+++ ccx.bat
@@ -1,2 +1,3 @@
 conda install -y -c conda-forge "libffi=3.3.*"
 conda install -y -c conda-forge opencv ffmpeg
+conda install -y -c conda-forge numpy
