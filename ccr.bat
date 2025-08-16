4394918eea3bae07fd6049258a9139ac4b2aef0b
Russell Wallace <russell.wallace@gmail.com>
2025-08-16 20:28:51 +0100

    Modified 1 file(s): ccr.bat

--- ccr.bat
+++ ccr.bat
@@ -1,9 +1,10 @@
-conda config --set channel_priority strict
+call conda config --set channel_priority strict
 call conda create --name %1 python=3.7 -y
 Rem should this specify 3.7.12?
-conda activate %1
-conda install -y -c conda-forge "libffi=3.3.*"
-conda install -y -c conda-forge opencv ffmpeg
-conda install -y -c conda-forge numpy
-conda install -y -c conda-forge opencv
-conda install -y -c conda-forge pyyaml tqdm Pillow scikit-learn 
+call conda activate %1
+call conda install -y -c conda-forge "libffi=3.3.*"
+call conda install -y -c pytorch -c conda-forge pytorch=1.12.1 torchvision=0.13.1 torchaudio=0.12.1 cpuonly
+call conda install -y -c conda-forge opencv ffmpeg
+call conda install -y -c conda-forge numpy
+call conda install -y -c conda-forge opencv
+call conda install -y -c conda-forge pyyaml tqdm Pillow scikit-learn 
