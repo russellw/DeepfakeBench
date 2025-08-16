4bf205b5da04a5ee1efa944fc1dae477b6975015
Russell Wallace <russell.wallace@gmail.com>
2025-08-16 11:44:38 +0100

    Modified 1 file(s): ccx.bat

--- ccx.bat
+++ ccx.bat
@@ -1,3 +1,5 @@
 conda install -y -c conda-forge "libffi=3.3.*"
 conda install -y -c conda-forge opencv ffmpeg
 conda install -y -c conda-forge numpy
+conda install -y -c conda-forge opencv
+conda install -y -c conda-forge pyyaml tqdm
