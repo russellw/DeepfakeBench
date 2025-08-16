aeb6925d5a43c1c287335b4808f9215279f96b35
Russell Wallace <russell.wallace@gmail.com>
2025-08-16 13:57:13 +0100

    Modified 1 file(s): ccr.bat; Deleted 1 file(s): ccx.bat

--- ccr.bat
+++ ccr.bat
@@ -2,4 +2,8 @@ conda config --set channel_priority strict
 call conda create --name %1 python=3.7 -y
 Rem should this specify 3.7.12?
 conda activate %1
-
+conda install -y -c conda-forge "libffi=3.3.*"
+conda install -y -c conda-forge opencv ffmpeg
+conda install -y -c conda-forge numpy
+conda install -y -c conda-forge opencv
+conda install -y -c conda-forge pyyaml tqdm Pillow scikit-learn 
--- ccx.bat
+++
@@ -1,5 +0,0 @@
-conda install -y -c conda-forge "libffi=3.3.*"
-conda install -y -c conda-forge opencv ffmpeg
-conda install -y -c conda-forge numpy
-conda install -y -c conda-forge opencv
-conda install -y -c conda-forge pyyaml tqdm Pillow scikit-learn 
